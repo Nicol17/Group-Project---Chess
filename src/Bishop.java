bb06937ca21f5e6a06f051b13aaee0bc451c6713
Yurie <yurie.koga.a@gmail.com>
2020-11-12 00:51:44 -0800

    merged current code

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Bishop extends Piece {
     ///////////////////////////////////////////////////
     protected static final int VALUE = 3;
@@ -7,12 +9,102 @@ public class Bishop extends Piece {
         super(VALUE, isWhite, position);
     }
 
+    /**
+     * Return a list of possible position for the piece user selected
+     *
+     * @param board    the current board from Game class
+     * @param selPiece the selected piece that user wants to move it
+     * @return
+     */
+    @Override
+    public ArrayList<Position> getPoss(Piece[][] board, Piece selPiece) {
+        final int row = selPiece.getPosition().getRow();
+        final int col = selPiece.getPosition().getCol();
+        int curRow;
+        int curCol;
+        ArrayList<Position> poss = new ArrayList<>();
+
+//        System.out.println("-- Bishop is called --");
+//        System.out.printf("selected (row, col): (%d, %d)%n", row, col);
+
+        // diagonal / left to right / up
+        curRow = row;
+        curCol = col;
+        while (0 < curRow && curRow < Game.BOARD_RANGE - 1 && 0 < curCol && curCol < Game.BOARD_RANGE - 1) {
+            Object[] result = checkDest(board, selPiece, ++curRow, ++curCol);
+            if (result[0] != null)
+                poss.add((Position) result[0]);
+            if (!result[1].equals(true))
+                break;
+        }
+
+        // diagonal / left to right / down
+        curRow = row;
+        curCol = col;
+        while (0 < curRow && curRow < Game.BOARD_RANGE - 1 && 0 < curCol && curCol < Game.BOARD_RANGE - 1) {
+            Object[] result = checkDest(board, selPiece, --curRow, ++curCol);
+            if (result[0] != null)
+                poss.add((Position) result[0]);
+            if (!result[1].equals(true))
+                break;
+        }
+
+        // diagonal / right to left / up
+        curRow = row;
+        curCol = col;
+        while (0 < curRow && curRow < Game.BOARD_RANGE - 1 && 0 < curCol && curCol < Game.BOARD_RANGE - 1) {
+            Object[] result = checkDest(board, selPiece, ++curRow, --curCol);
+            if (result[0] != null)
+                poss.add((Position) result[0]);
+            if (!result[1].equals(true))
+                break;
+        }
+
+        // diagonal / right to left / down
+        curRow = row;
+        curCol = col;
+        while (0 < curRow && curRow < Game.BOARD_RANGE - 1 && 0 < curCol && curCol < Game.BOARD_RANGE - 1) {
+            Object[] result = checkDest(board, selPiece, --curRow, --curCol);
+            if (result[0] != null)
+                poss.add((Position) result[0]);
+            if (!result[1].equals(true))
+                break;
+        }
+        return poss;
+    }
+
+    /**
+     * Return a result if {@param piece} can move to Position({@param targetRow}, {@param targetCol}).
+     *
+     * @param board     the current board from Game class
+     * @param selPiece  the selected piece that user wants to move it
+     * @param targetRow row of Position to check if {@param piece} can move to
+     * @param targetCol column of Position to check if {@param piece} can move to
+     * @return Object[] of result:
+     *         - [0]: Position if {@param piece} can move to. otherwise, null
+     *         - [1]: boolean - true:continue checking, false:stop checking
+     */
+    private Object[] checkDest(Piece[][] board, Piece selPiece, int targetRow, int targetCol) {
+        Object[] result = new Object[]{
+                null, false
+        };
+        if (board[targetRow][targetCol] == null) {
+            // no piece: can move and continue
+            result[0] = new Position(targetRow, targetCol);
+            result[1] = true;
+        } else {
+            if (board[targetRow][targetCol].isWhite() == selPiece.isWhite()) {
+                // same color: cannot move and stop
+            } else {
+                // different color: can move and stop
+                result[0] = new Position(targetRow, targetCol);
+            }
+        }
+        return result;
+    }
+
     @Override
     public void move(Position newPosition) {
-        // if there is another piece is exist in the destiny position, should stop one block before
-        // better to loop to increase row and col till the destiny, and set at the maximum position.
-
-        setPosition(newPosition);
         System.out.println("Moved Diagonally");
     }
 
@@ -29,17 +121,17 @@ public class Bishop extends Piece {
         if (!super.isValidMove(newPosition))
             return false;
         return (Math.abs(this.position.getRow() - newPosition.getRow())
-            == Math.abs(this.position.getCol() - newPosition.getCol()));
+                == Math.abs(this.position.getCol() - newPosition.getCol()));
     }
 
     @Override
     public String toString() {
         String color = (isWhite()) ? "White" : "Black";
         return "Bishop{" +
-            "value=" + getValue() +
-            ", color=" + color +
-            ", position=(" + getPosition().getRow() + ", " + getPosition().getCol() + ')' +
-            '}';
+                "value=" + getValue() +
+                ", color=" + color +
+                ", position=(" + getPosition().getRow() + ", " + getPosition().getCol() + ')' +
+                '}';
     }
 
 
--- src/King.java
+++ src/King.java
@@ -1,11 +1,83 @@
+import java.util.ArrayList;
+
 public class King extends Piece {
     ///////////////////////////////////////////////////
     protected static final int VALUE = 1_000;
     ///////////////////////////////////////////////////
 
-
-    public King( boolean isWhite, Position position) {
+    public King(boolean isWhite, Position position) {
         super(VALUE, isWhite, position);
+    }
+
+    /**
+     * Return a list of possible position for the piece user selected
+     *
+     * @param board    the current board from Game class
+     * @param selPiece the selected piece that user wants to move it
+     * @return
+     */
+    @Override
+    public ArrayList<Position> getPoss(Piece[][] board, Piece selPiece) {
+        final int row = selPiece.getPosition().getRow();
+        final int col = selPiece.getPosition().getCol();
+        int curRow;
+        int curCol;
+        ArrayList<Position> psb = new ArrayList<>();
+
+        System.out.printf("(row,col): (%d,%d)%n", row, col);
+        System.out.printf("selected piece: %s%n", board[row][col]);
+        System.out.println("--- possibility ---");
+
+        Position position = checkForKing(board, board[row][col], row, col + 1);
+        if (position != null)
+            psb.add(position);
+        position = checkForKing(board, board[row][col], row, col - 1);
+        if (position != null) {
+            psb.add(position);
+        }
+        position = checkForKing(board, board[row][col], row + 1, col);
+        if (position != null) {
+            psb.add(position);
+        }
+        position = checkForKing(board, board[row][col], row - 1, col);
+        if (position != null) {
+            psb.add(position);
+        }
+        position = checkForKing(board, board[row][col], row + 1, col + 1);
+        if (position != null) {
+            psb.add(position);
+        }
+        position = checkForKing(board, board[row][col], row + 1, col - 1);
+        if (position != null) {
+            psb.add(position);
+        }
+        position = checkForKing(board, board[row][col], row - 1, col + 1);
+        if (position != null) {
+            psb.add(position);
+        }
+        position = checkForKing(board, board[row][col], row - 1, col - 1);
+        if (position != null) {
+            psb.add(position);
+        }
+        return psb;
+    }
+
+    private Position checkForKing(Piece[][] board, Piece king, int targetRow, int targetCol) {
+        if (board[targetRow][targetCol] == null) {
+//            psb.add(new Position(targetRow, targetCol));
+            System.out.printf("(row,col): (%d,%d)%n", targetRow, targetCol);
+            return new Position(targetRow, targetCol);
+        } else {
+            if (board[targetRow][targetCol].isWhite() == king.isWhite()) {
+                return null;
+            } else {
+                //can move
+//                psb.add(new Position(targetRow, targetCol));
+                System.out.printf("(row,col): (%d,%d)%n", targetRow, targetCol);
+                System.out.println("different color");
+                return new Position(targetRow, targetCol);
+            }
+        }
     }
 
     @Override
@@ -14,20 +86,20 @@ public class King extends Piece {
     }
 
     @Override
-    public boolean isValidMove (Position newPosition){
-        if (!super.isValidMove(position)){
+    public boolean isValidMove(Position newPosition) {
+        if (!super.isValidMove(position)) {
             return false;
         }
-        if (newPosition.getCol() ==  (this.position.getCol() +1) && (newPosition.getRow() == this.position.getRow()) ||
-            newPosition.getCol() ==  (this.position.getCol() -1) && (newPosition.getRow() == this.position.getRow()) ||
-            newPosition.getCol() ==  (this.position.getCol()) && newPosition.getRow() == (this.position.getRow() +1) ||
-            newPosition.getCol() ==  (this.position.getCol()) && newPosition.getRow() == (this.position.getRow() -1) ||
-            newPosition.getCol() ==  (this.position.getCol() + 1) && newPosition.getRow() == (this.position.getRow() + 1) ||
-            newPosition.getCol() ==  (this.position.getCol() - 1) && newPosition.getRow() == (this.position.getRow() + 1) ||
-            newPosition.getCol() ==  (this.position.getCol() + 1) && newPosition.getRow() == (this.position.getRow() -1) ||
-            newPosition.getCol() ==  (this.position.getCol() - 1) && newPosition.getRow() == (this.position.getRow() -1)) {
+        if (newPosition.getCol() == (this.position.getCol() + 1) && (newPosition.getRow() == this.position.getRow()) ||
+                newPosition.getCol() == (this.position.getCol() - 1) && (newPosition.getRow() == this.position.getRow()) ||
+                newPosition.getCol() == (this.position.getCol()) && newPosition.getRow() == (this.position.getRow() + 1) ||
+                newPosition.getCol() == (this.position.getCol()) && newPosition.getRow() == (this.position.getRow() - 1) ||
+                newPosition.getCol() == (this.position.getCol() + 1) && newPosition.getRow() == (this.position.getRow() + 1) ||
+                newPosition.getCol() == (this.position.getCol() - 1) && newPosition.getRow() == (this.position.getRow() + 1) ||
+                newPosition.getCol() == (this.position.getCol() + 1) && newPosition.getRow() == (this.position.getRow() - 1) ||
+                newPosition.getCol() == (this.position.getCol() - 1) && newPosition.getRow() == (this.position.getRow() - 1)) {
             return true;
-        }  else {
+        } else {
             return false;
         }
     }
@@ -37,9 +109,9 @@ public class King extends Piece {
     public String toString() {
         String color = (isWhite()) ? "White" : "Black";
         return "King{" +
-            "value=" + getValue() +
-            ", color=" + color +
-            ", position=(" + getPosition().getRow() + ", " + getPosition().getCol() + ')' +
-            '}';
+                "value=" + getValue() +
+                ", color=" + color +
+                ", position=(" + getPosition().getRow() + ", " + getPosition().getCol() + ')' +
+                '}';
     }
 }
